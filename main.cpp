80db4229dd084ea227d4db24731980df3047eb95
lethic <lethiic@gmail.com>
2015-02-23 02:18:33 -0800

    add main file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: Lethic 
+	> Mail: lethiic@gmail.com 
+	> Created Time: Mon 23 Feb 02:04:52 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
